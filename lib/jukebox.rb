0ec918133fa942c16a5c8c55e0d6bb5ec6695c1b
Matthew Sherman <msherman0801@gmail.com>
2018-11-11 01:38:19 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -14,3 +14,4 @@ def help
   puts "I accept the following commands:"
   puts "- help : displays this help message"
   puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
